1201845422cce1dc87cef37ac4067ee6b657d9cc
Robert Wagner <rwwagner90@gmail.com>
2018-03-02 11:52:43 -0500

    Initial functionality

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const fs = require('fs-extra');
+
 module.exports = {
-  name: 'ember-cli-netlify'
+  name: 'ember-cli-netlify',
+  outputReady(result) {
+    fs.copySync('.netlifyheaders', 'dist/_headers', { clobber: true });
+    fs.copySync('.netlifyredirects', 'dist/_redirects', { clobber: true });
+  }
 };
