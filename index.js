351f6aa4b614df62c25dd8e8583f2f1f24c01fa0
Robert Wagner <rwwagner90@gmail.com>
2019-06-19 15:45:56 -0400

    Refactor file writing

--- index.js
+++ index.js
@@ -18,25 +18,19 @@ module.exports = {
 
     if (fs.pathExistsSync('.netlifyredirects')) {
       fs.copySync('.netlifyredirects', 'dist/_redirects', { clobber: true });
-    } else if (netlifyOptions && netlifyOptions.redirects) {
-      fs.writeFileSync('dist/_redirects');
     }
-
-    const stream = fs.createWriteStream('dist/_redirects', { flags: 'a' });
 
     if (netlifyOptions && netlifyOptions.redirects) {
       netlifyOptions.redirects.forEach((redirect) => {
-        stream.write(`${redirect}\n`);
+        writeToFile('dist/_redirects', redirect);
       });
     }
 
     if (redirectsFromPlugins && redirectsFromPlugins.length) {
       redirectsFromPlugins.forEach((redirect) => {
-        stream.write(`${redirect}\n`);
+        writeToFile('dist/_redirects', redirect);
       });
     }
-
-    stream.end();
   }
 };
 
@@ -47,4 +41,13 @@ function loadNetlifyRedirects(context) {
     .filter((addon) => addon.pkg.keywords.includes('ember-cli-netlify-plugin'))
     .filter((addon) => typeof addon.netlifyRedirects === 'function')
     .map((addon) => addon.netlifyRedirects.bind(addon));
+}
+
+/**
+ * Write lines of text to a file
+ * @param {string} file The file path
+ * @param {string} text The text to write
+ */
+function writeToFile(file, text) {
+  fs.outputFileSync(file, `${text}\n`, { flag: 'a' });
 }
